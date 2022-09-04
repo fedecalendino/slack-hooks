2558cef542151b796870fd9a6355cee6b7d0279a
Fede Calendino <fede@calendino.com>
2022-09-04 14:21:05 +0200

    Add ImageAccesory

--- webhooks/blocks/accessory.py
+++ webhooks/blocks/accessory.py
@@ -11,11 +11,11 @@ class Accessory:
         }
 
 
-class Button(Accessory):
+class ButtonAccessory(Accessory):
     def __init__(self, text: PlainText, action_id: str, value: str, url: str = None):
         super().__init__("button")
 
-        self.text: PlainText = text
+        self.text = text
         self.value: str = value
         self.action_id: str = action_id
         self.url: str = url
@@ -28,6 +28,24 @@ class Button(Accessory):
             "url": self.url,
             "action_id": self.action_id,
             "value": self.value,
+        }
+
+        return serialization
+
+
+class ImageAccessory(Accessory):
+    def __init__(self, image_url: str, alt_text: str = ""):
+        super().__init__("image")
+
+        self.image_url: str = image_url
+        self.alt_text: str = alt_text
+
+    def serialize(self) -> dict:
+        serialization = super().serialize()
+
+        serialization |= {
+            "image_url": self.image_url,
+            "alt_text": self.alt_text,
         }
 
         return serialization
